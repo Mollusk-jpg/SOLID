434d5992b5388675adefd26564f6ff69f01c5950
Mollusk-jpg <d.leivdal2@newcastle.ac.uk>
2026-06-07 13:16:50 +0100

    Update Blog.tsx

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -1,8 +1,15 @@
 import type { FunctionComponent } from "react";
-import { MakePost } from "./MakePost.tsx";
-import { Post } from "./Post.tsx";
+import { MakePost } from "./MakePost";
+import { Post } from "./Post";
+import { useSolidAuth } from "@ldo/solid-react";
 
 export const Blog: FunctionComponent = () => {
+  const { session } = useSolidAuth();
+  // Only show blog posts if user is logged in. 
+  if (!session.isLoggedIn) {
+    return <p>Please log in to see your blog.</p>;
+  }
+
   return (
     <main>
       <MakePost />
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,21 +3,22 @@ import { useState } from "react";
 import type { FunctionComponent } from "react";
 
 export const Header: FunctionComponent = () => {
-    const { session, login, logout } = useSolidAuth();
-    const [issuer, setIssuer] = useState("https://solidcommunity.net");
+const { session, login, logout } = useSolidAuth(); 
+const [issuer, setIssuer] = useState("https://solidcommunity.net");
+
 return (
     <header>
-    {session.isLoggedIn ? (
+        {session.isLoggedIn ? (
         // If the user is logged in
         <p>
-        Logged in as {session.webId}.{" "}
-        <button onClick={logout}>Log Out</button>
+            Logged in as {session.webId}.{" "}
+            <button onClick={logout}>Log Out</button>
         </p>
-    ) : (
+        ) : (
         // If the user is not logged in
         <div>
-        <p>You are not logged in.</p>
-        <input 
+            <p>You are not logged in.</p>
+            <input 
             type="text" 
             value={issuer} 
             onChange={(e) => setIssuer(e.target.value)} 
